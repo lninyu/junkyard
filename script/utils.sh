814142257e26f28cc0942ada64949f99a17d8937
lninyu <lninyurir@gmail.com>
2025-06-28 18:46:05 +0900

    add utf8Length(), graycode(), toGraycode()

--- script/utils.sh
+++ script/utils.sh
@@ -1,4 +1,4 @@
-((__utils__))&&return||readonly __utils__=1
+((__utils__)) && return 1 || readonly __utils__=1
 
 function utils.sleep() {
     read -srt "${1:?}" _
@@ -8,5 +8,33 @@ function utils.hasFunc() {
     declare -F "${1:?}" &> /dev/null
 }
 
+function utils.utf8Length() {
+    local -n result="${1:?}"; result=-1
+    local -r string="${2:?}" IFS= LC_ALL=C
+    local c
+
+    while read -rn1 -d "" c; do
+        printf -v c %d "'${c}"
+
+        (((c & 0xc0) ^ 0x80 && ++result))
+    done <<< "${string}"
+}
+
+function utils.graycode() {
+    local -n result="${1:?}"
+    local -i number="${2:?}"
+
+    ((result = number ^ number >> 1))
+}
+
+function utils.toGraycode() {
+    local -n number="${1:?}"
+
+    ((number ^= number >> 1))
+}
+
 readonly -f utils.sleep
 readonly -f utils.hasFunc
+readonly -f utils.utf8Length
+readonly -f utils.graycode
+readonly -f utils.toGraycode
