36b15b158acfb8a5af2aa56a2ccda27a1e0907be
lninyu <lninyurir@gmail.com>
2025-06-29 00:55:21 +0900

    utils.sh: 実装追加, ドキュメント更新 listEntries(): 新規実装 @args追加

--- script/utils.sh
+++ script/utils.sh
@@ -4,7 +4,11 @@
 # - sleep (100% builtin fr)
 #
 # @recommended >= 4.0-alpha
-# - read -t can handle fractional seconds
+# - read -t can handle fractional seconds.
+#
+# @args
+# - $1 seconds
+#   - Number of seconds (can be fractional) to sleep.
 function utils.sleep() {
     read -srt "${1:?}" _
 }
@@ -12,6 +16,13 @@ function utils.sleep() {
 # @summary
 # - Check if function is defined.
 # - You know it when you see it.
+#
+# @args
+# - $1 fname
+#   - Name of the function to check for existence.
+#
+# @status
+# - 0 if the function exists, non-zero otherwise.
 function utils.funcExists() {
     declare -F "${1:?}" &> /dev/null
 }
@@ -47,6 +58,12 @@ function utils.utf8Length() {
 # @summary
 # - Converts number to graycode.
 #
+# @args
+# - $1 result
+#   - Name of the variable to store the graycode result.
+# - $2 number
+#   - The input number to convert.
+#
 # @example
 # - declare result number=10
 # - utils.graycode result number
@@ -60,12 +77,20 @@ function utils.graycode() {
 
 # @summary
 # - Converts number to graycode in-place.
+#
+# @args
+# - $1 number
+#   - The variable name holding the number to convert (converted in-place).
 function utils.toGraycode() {
     ((${1:?} ^= ${1} >> 1))
 }
 
 # @summary
-# - Make the specified function immutable.
+# - Make the specified function(s) immutable.
+#
+# @args
+# - $@ fname...
+#   - One or more function names to freeze (mark readonly).
 function utils.freezeFunc() {
     local list; for list; do
         readonly -f "${list}"
@@ -110,4 +135,54 @@ function utils.getFuncs() {
     elif utils.funcExists "${cond}"; then
         list=("${cond}")
     fi
+}
+
+# @summary
+# - Recursively lists all files and directories under a given path.
+#
+# @remark
+# - Symbolic links to directories are not traversed to avoid infinite recursion.
+# - The result includes the initial directory itself as the first element.
+#
+# @require >= 4.3
+# - local -n
+#
+# @args
+# - $1 result
+#   - Name of an array variable (passed by name) to store the results.
+# - $2 path
+#   - Directory path to start listing from.
+#
+# @example "List all entries recursively starting from /tmp/mydir."
+# - declare -a entries
+# - utils.listEntries entries "/tmp/mydir"
+# - printf '%s\n' "${entries[@]}"
+#
+# @example:output
+# - /tmp/mydir
+# - /tmp/mydir/file1.txt
+# - /tmp/mydir/subdir
+# - /tmp/mydir/subdir/file2.txt
+function utils.listEntries() {
+    local -n result="${1:?}"
+    local -r path="${2:?}"
+    local entry
+
+    if [[ ${FUNCNAME[0]} != "${FUNCNAME[1]}" ]]; then
+        result=("${path}")
+    fi
+
+    for entry in "${path}"/*; do
+        if [[ -f "${entry}" ]]; then
+            result+=("${entry}")
+        elif [[ -d "${entry}" ]] && ! [[ -L "${entry}" ]]; then
+            result+=("${entry}")
+
+            "${FUNCNAME}" "${1}" "${entry}"
+        fi
+    done
+}
+
+function utils.table() {
+    :
 }
