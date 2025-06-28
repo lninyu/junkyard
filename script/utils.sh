9ea584c4b6954b5072115473480437288c21ba52
lninyu <lninyurir@gmail.com>
2025-06-28 23:18:54 +0900

    utils.sh: 関数整理・実装追加・ドキュメント更新
    - hasFunc() を funcExists() に変更
    - utf8Length(): 不要な処理を削減
    - graycode(), toGraycode(): 不要な処理を削減し互換性向上
    - freezeFunc(), getFuncs(): 新規実装
    - 各関数にドキュメント(もどき)を追加
    
    やり過ぎだって

--- script/utils.sh
+++ script/utils.sh
@@ -1,13 +1,37 @@
 ((__utils__)) && return 1 || readonly __utils__=1
 
+# @summary
+# - sleep (100% builtin fr)
+#
+# @recommended >= 4.0-alpha
+# - read -t can handle fractional seconds
 function utils.sleep() {
     read -srt "${1:?}" _
 }
 
-function utils.hasFunc() {
+# @summary
+# - Check if function is defined.
+# - You know it when you see it.
+function utils.funcExists() {
     declare -F "${1:?}" &> /dev/null
 }
 
+# @summary
+# - Counts the number of characters in utf-8 strings.
+#
+# @remark
+# - Counts UTF-8 lead bytes only; does not handle grapheme clusters correctly.
+#
+# @require >= 4.3
+# - local -n
+#
+# @example "Count the number of UTF-8 characters in a string containing emoji."
+# - declare length string=$'wow\xf0\x9f\x92\xa9!'
+# - utils.utf8Length length "${string}"
+# - echo "length = ${length}"
+#
+# @example:output
+# - length = 5
 function utils.utf8Length() {
     local -n result="${1:?}"; result=-1
     local -r string="${2:?}" IFS= LC_ALL=C
@@ -16,25 +40,74 @@ function utils.utf8Length() {
     while read -rn1 -d "" c; do
         printf -v c %d "'${c}"
 
-        (((c & 0xc0) ^ 0x80 && ++result))
+        ((c & 0xc0 ^ 0x80 && ++result))
     done <<< "${string}"
 }
 
+# @summary
+# - Converts number to graycode.
+#
+# @example
+# - declare result number=10
+# - utils.graycode result number
+# - echo "${number} -> ${result}"
+#
+# @example:output
+# - 10 -> 15
 function utils.graycode() {
-    local -n result="${1:?}"
-    local -i number="${2:?}"
-
-    ((result = number ^ number >> 1))
+    ((${1:?} = ${2:?} ^ ${2} >> 1))
 }
 
+# @summary
+# - Converts number to graycode in-place.
 function utils.toGraycode() {
-    local -n number="${1:?}"
-
-    ((number ^= number >> 1))
+    ((${1:?} ^= ${1} >> 1))
 }
 
-readonly -f utils.sleep
-readonly -f utils.hasFunc
-readonly -f utils.utf8Length
-readonly -f utils.graycode
-readonly -f utils.toGraycode
+# @summary
+# - Make the specified function immutable.
+function utils.freezeFunc() {
+    local list; for list; do
+        readonly -f "${list}"
+    done &> /dev/null
+}
+
+# @summary
+# - List function names matching a pattern or specific name.
+#
+# @require >= 4.3
+# - local -n
+#
+# @example "Get functions matching a pattern."
+# - declare -a funcs
+# - utils.getFuncs funcs "utils.*f*"
+# - printf "%s\n" "${funcs[@]}"
+#
+# @example:output
+# - utils.freezeFunc
+# - utils.funcExists
+# - utils.utf8Length
+function utils.getFuncs() {
+    local -n list="${1:?}"
+    local -r cond="${2}" IFS=$'\n'
+
+    if [[ -z ${cond} ]]; then
+        list=($(declare -F))
+        list=(${list[@]##* })
+    elif [[ ${cond} == *"*"* ]]; then
+        local -i index size
+
+        list=($(declare -F))
+        list=(${list[@]##* })
+
+        for ((index = 0, size = ${#list[@]}; index < size; ++index)); do
+            if [[ "${list[index]}" != ${cond} ]]; then
+                list[index]=
+            fi
+        done
+
+        list=(${list[@]})
+    elif utils.funcExists "${cond}"; then
+        list=("${cond}")
+    fi
+}
