24994294cb42fde3a90fd39007801edf2a1e9803
lninyu <lninyurir@gmail.com>
2025-06-29 22:19:48 +0900

    list.new

--- script/list.sh
+++ script/list.sh
@@ -1,6 +1,11 @@
 ((__list__)) && return 1 || readonly __list__=1818850164 #0x6c697374
 # list:{0x6c697374, limit:int, count:int, meta:(str){offset:int, length:int}[], data:str[]}
-function list.new() { :;}
+function list.new() {
+    local -n self="${1:?}"
+
+    self=(${__list__} $((${2:-0} > 0 ? ${2:-0} : 0)) 0 "")
+}
+
 function list.append() { :;}
 function list.appendRaw() { :;}
 function list.insert() { :;}
