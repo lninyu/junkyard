fe769ca283343505ce1ffebf180781dac83397f5
lninyu <59942911+lninyu@users.noreply.github.com>
2025-07-05 00:58:29 +0900

    Update aaa.sh

--- tui/src/aaa.sh
+++ tui/src/aaa.sh
@@ -4,91 +4,90 @@ function tui.input() {
     while read -srn1 -d "" a; do
         case ${a} in
         $'\x1b')
-            b+=($'\x1b')
+            b+=$'\x1b'
             read -srn1 -d "" a
 
             case ${a} in
             $'\x5b')
-                b+=($'\x5b')
+                b+=$'\x5b'
                 read -srn1 -d "" a
 
                 case ${a} in
                 $'\x32')
-                    b+=($'\x32')
+                    b+=$'\x32'
                     read -srn1 -d "" a
 
                     case ${a} in
                     $'\x30')
-                        b+=($'\x30')
+                        b+=$'\x30'
                         read -srn1 -d "" a
 
                         case ${a} in
                         $'\x30')
-                            b+=($'\x30')
+                            b+=$'\x30'
                             read -srn1 -d "" a
 
                             case ${a} in
                             $'\x7e')
                                 b=()
                                 while read -srn1 -d "" a; do
-                                    case ${b[*]} in
+                                    b+="${a}"
+
+                                    case ${b} in
                                     *$'\x1b\x5b\x32\x30\x31\x7e')
-                                        echo -n "${b[*]::${#b[*]}-6}"
-                                        b=()
+                                        echo -n "${b::${#b}-6}" | xxd
+                                        b=
                                         break
-                                    ;;
-                                    *)
-                                        b+=("${a}")
                                     ;;
                                     esac
                                 done
                             ;;
                             *)
-                                echo -n "${b[*]}${a}"
+                                echo -n "${b}${a}" | xxd
                             ;;
                             esac
                         ;;
                         *)
-                            echo -n "${b[*]}${a}"
-                            b=()
+                            echo -n "${b}${a}" | xxd
+                            b=
                         ;;
                         esac
                     ;;
                     *)
-                        echo -n "${b[*]}${a}"
-                        b=()
+                        echo -n "${b}${a}" | xxd
+                        b=
                     ;;
                     esac
                 ;;
                 $'\x3c')
-                    b+=($'\x3c')
+                    b+=$'\x3c'
                     while read -srn1 -d "" a; do
                         case ${a} in
                         [Mm])
-                            echo -n "${b[*]}${a}"
-                            b=()
+                            echo -n "${b}${a}" | xxd
+                            b=
                             break
                         ;;
                         *)
-                            b+=("${a}")
+                            b+="${a}"
                         ;;
                         esac
                     done
                 ;;
                 *)
-                    echo -n "${b[*]}${a}"
-                    b=()
+                    echo -n "${b}${a}" | xxd
+                    b=
                 ;;
                 esac
             ;;
             *)
-                echo -n "${b[*]}${a}"
-                b=()
+                echo -n "${b}${a}" | xxd
+                b=
             ;;
             esac
         ;;
         *)
-            echo -n "${a}"
+            echo -n "${a}" | xxd
         ;;
         esac
     done
