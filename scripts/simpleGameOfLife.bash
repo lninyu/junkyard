4d9c5964cdc18c737bcc35c1a01e46b37ae9d49c
lninyu <59942911+lninyu@users.noreply.github.com>
2025-07-31 09:49:16 +0900

    Update simpleGameOfLife.bash

--- scripts/simpleGameOfLife.bash
+++ scripts/simpleGameOfLife.bash
@@ -3,9 +3,9 @@ step() {
     # j  tl,      , k  tr, l  cl,      , m  cr, n  bl,      , o  br
     # p ~tl, q ~tc, r ~tr, s ~cl, t ~cc, u ~cr, v ~bl, w ~bc, x ~br
     local -i {a..x}
-    ((q= ~$8,p= ~(j=$8<<1|1&$3>>63),r= ~(k=0x7fffffffffffffff&$8>>1|$1<<63)))
-    ((t= ~$5,s= ~(l=$5<<1|1&$6>>63),u= ~(m=0x7fffffffffffffff&$5>>1|$4<<63)))
-    ((w= ~$2,v= ~(n=$2<<1|1&$9>>63),x= ~(o=0x7fffffffffffffff&$2>>1|$7<<63)))
+    ((q=~$8,p=~(j=$8<<1|1&$3>>63),r=~(k=0x7fffffffffffffff&$8>>1|$1<<63)))
+    ((t=~$5,s=~(l=$5<<1|1&$6>>63),u=~(m=0x7fffffffffffffff&$5>>1|$4<<63)))
+    ((w=~$2,v=~(n=$2<<1|1&$9>>63),x=~(o=0x7fffffffffffffff&$2>>1|$7<<63)))
     ((i=(h=(g=(f=(e=(d=(c=j&$8)&k)&l)&m)&n)&$2)&o))
     ((h=h&x|(g=g&w|(f=f&v|(e=e&u|(d=d&s|(c=c&r|(b=j^$8)&k)&l)&m)&n)&$2)&o))
     ((g=g&x|(f=f&w|(e=e&v|(d=d&u|(c=c&s|(b=b&r|(a=p&q)&k)&l)&m)&n)&$2)&o))
