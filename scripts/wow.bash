d5ee197f621214596d12d82a807cfec46a225742
lninyu <59942911+lninyu@users.noreply.github.com>
2025-08-02 16:28:01 +0900

    Update wow.bash

--- scripts/wow.bash
+++ scripts/wow.bash
@@ -1,6 +1,9 @@
-aaaaa() {
-    local -r LC_ALL=C IFS=
+declare -r LC_ALL=C IFS=
+
+if (($#)); then
+    echo $(($*))
+else
     while read -r; do
         ( ((REPLY)) ) 2> /dev/null && echo "= $((REPLY))"
     done
-}
+fi
