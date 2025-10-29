c007e064a60bbc8c3440c58513e8829fdeba114e
lninyu <59942911+lninyu@users.noreply.github.com>
2025-10-29 16:15:42 +0900

    Add sparse.shiftCopy function to handle array copying

--- script/sparseClone.bash
+++ script/sparseClone.bash
@@ -3,3 +3,13 @@ function sparseClone() {
     builtin : "$(declare -p "${2:?}")"
     eval "${1:?}=${_#*=}"
 }
+
+sparse.shiftCopy() {
+    local -n a=${1:?} b=${2:?}
+    local -i c=${3:?}
+
+    for c in "${!b[@]}"; do
+        # shellcheck disable=SC2034
+        a[c+$3]="${b[c]}"
+    done
+}
