b5678878c6b4eede2810c9ee27222f3e42f3c43a
lninyu <59942911+lninyu@users.noreply.github.com>
2025-10-29 18:31:23 +0900

    Add sparse.clone and sparse.copy functions
    💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩💩

--- script/sparseClone.bash
+++ script/sparseClone.bash
@@ -22,3 +22,27 @@ sparse.shiftCopyIfEmpty() {
         builtin : "${a[c+$3]:="${b[c]}"}"
     done
 }
+
+sparse.clone() { # (src, src_pos, dest, dest_offset, length)
+    local -n a=${1:?} b=${3:?}
+    local -i c=${4:?} d=("${!a[@]}")
+
+    for c in "${d[@]:${2:?}:${5:?}}"; do
+        b[c+$4]="${a[c]}"
+    done
+}
+
+sparse.copy() { # (src, src_offset, dest, dest_offset, length)
+    local -n a=${1:?} b=${3:?}
+    local -i c=${2:?} d=${5:?}+$2 e=${4:?}-$2 f
+
+    for f in "${!a[@]}"; do
+        if (( c <= f )); then
+            if (( d <= f )); then
+                break
+            fi
+
+            b[f+e]="${a[f]}"
+        fi
+    done
+}
