fe3ddaf3142e9e26c82a1657102ff6826c9a9732
lninyu <59942911+lninyu@users.noreply.github.com>
2025-12-11 16:18:20 +0900

    Refactor sparse::bounds function for clarity

--- script/sparse.bash
+++ script/sparse.bash
@@ -1,25 +1,24 @@
-# # version(Bash >= 5.1)
-# sparse::bounds <list> <minidx> <maxidx>
 function sparse::bounds() {
-    local IFS=" " LC_ALL=C a
+    local IFS=$'\40' LC_ALL=C a
 
     [[ ${!1@a} == *a* ]] || return 1
 
     # If $1 == parameter, then `local -n parameter=$1` would create
     # a circular reference: parameter -> parameter.
-    if [[ ${1:?} == b ]]; then
-        # shellcheck disable=2154
-        a="${!b[*]}" # direct
-    else
-        local -n c=${1}
-        a="${!c[*]}"
+    if [[ ${1:?} != b ]]; then
+        local -n b=${1}
     fi
 
-    ((${#a[@]})) || return 1
+    ((${#b[@]})) || return 1
+
+    # "idx idx2 idx3 ..."
+    a="${!b[*]}"
 
     printf -v "${2:?}" %d "${a%% *}"
 
-    # [2 ** 63] wraps to [0], 2 ** 63 is 19-digit.
+    # [2 ** 63] wraps to [0], 2 ** 63 - 1 is 19-digit.
+    # local -ir length=2**63-1
+    # a="${a:${#a}<${#length}?0:-${#length}}"
     a="${a:${#a}<19?0:-19}"
 
     printf -v "${3:?}" %d "${a##* }"
