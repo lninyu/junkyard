3f23f9edaa3b0ea9a4ed8f568d4f5f20ff08b8bb
lninyu <59942911+lninyu@users.noreply.github.com>
2025-12-11 14:18:20 +0900

    aaaaa

---
+++ script/sparse.bash
@@ -0,0 +1,20 @@
+function sparse::bounds() {
+    local IFS=" " LC_ALL=C a
+
+    # If $1 == parameter, then `local -n parameter=$1` would create
+    # a circular reference: parameter -> parameter.
+    if [[ ${1:?} == b ]]; then
+        # shellcheck disable=2154
+        a="${!b[*]}" # direct
+    else
+        local -n c=${1}
+        a="${!c[*]}"
+    fi
+
+    printf -v "${2:?}" %d "${a%% *}"
+
+    # [2 ** 63] wraps to [0], 2 ** 63 is 19-digit.
+    a="${a: -19}"
+
+    printf -v "${3:?}" %d "${a##* }"
+}
