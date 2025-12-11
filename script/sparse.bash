c6178fc7a2774cba72ed83460524ec8545107874
lninyu <59942911+lninyu@users.noreply.github.com>
2025-12-11 14:56:35 +0900

    Update Bash version requirement and fix return values
    afewfwefwefwe

--- script/sparse.bash
+++ script/sparse.bash
@@ -1,19 +1,21 @@
-# # version(Bash >= 4.3)
+# # version(Bash >= 5.1)
 # sparse::bounds <list> <minidx> <maxidx>
 function sparse::bounds() {
     local IFS=" " LC_ALL=C a
 
+    [[ ${!1@a} == *a* ]] || return 1
+
     # If $1 == parameter, then `local -n parameter=$1` would create
     # a circular reference: parameter -> parameter.
     if [[ ${1:?} == b ]]; then
-        ((${#b[@]})) || return
+        ((${#b[@]})) || return 1
 
         # shellcheck disable=2154
         a="${!b[*]}" # direct
     else
         local -n c=${1}
 
-        ((${#c[@]})) || return
+        ((${#c[@]})) || return 1
 
         a="${!c[*]}"
     fi
